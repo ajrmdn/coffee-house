16afb71811f5dae19c998f91bc4f8954c15010b2
Abdellah Ramadan <ajrmdn@gmail.com>
2019-09-06 09:58:27 -0700

    add webpack and will add a components subfolder.

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 ReactDOM.render(
   <div>
     <h1>Welcome to Lakes Cold Brews!</h1>
-    <h3>Morning Brew</h3>
-    <h3>Waking up with Lake</h3>
-    <h3>Blake Brews</h3>
-    <h3>$3</h3>
+    <h3>Name: Morning Brew</h3>
+    <h3>Flavor: Waking up with Lake</h3>
+    <h3>Brand: Blake Brews</h3>
+    <h3>Price: $3</h3>
   </div>,
   document.getElementById('react-app-root')
 );
