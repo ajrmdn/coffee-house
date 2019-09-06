457f7e7c797e64f48d58439a8b54ada229645cf8
Abdellah Ramadan <ajrmdn@gmail.com>
2019-09-06 14:57:26 -0700

    continue to work on my code. Hot loader will not work.

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,7 @@ plugins: [
   new HtmlWebpackPlugin({
     template:'template.ejs',
     appMountId: 'react-app-root',
-    title: 'Coffee-House',
+    title: 'Coffee House',
     filename: resolve(__dirname, "build", "index.html"),
   }),
  ]
