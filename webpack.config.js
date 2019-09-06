422b05fc0e282224b9161c51f61fc3e50308bc75
Abdellah Ramadan <ajrmdn@gmail.com>
2019-09-06 16:01:07 -0700

    continue to debug my code. I got apm to finally install but i still cant get my website to load.

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,7 @@ plugins: [
   new HtmlWebpackPlugin({
     template:'template.ejs',
     appMountId: 'react-app-root',
-    title: 'coffee house',
+    title: 'Coffee House',
     filename: resolve(__dirname, "build", "index.html"),
   }),
  ]
