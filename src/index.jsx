d6d9550f57782a8aaf1dd460dfa2ec1caae76bc5
Abdellah Ramadan <ajrmdn@gmail.com>
2019-09-06 09:00:45 -0700

    add jsx to my app.

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+ReactDOM.render(
+  <div>
+    <h1>Welcome to Lakes Cold Brews!</h1>
+    <h3>Morning Brew</h3>
+    <h3>Waking up with Lake</h3>
+    <h3>Blake Brews</h3>
+    <h3>$3</h3>
+  </div>,
+  document.getElementById('react-app-root')
+);
