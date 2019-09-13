1f2e3c52975f7925efd36280ed0131d52d123161
Abdellah Ramadan <ajrmdn@gmail.com>
2019-09-13 11:04:59 -0700

    refactor my code to try and debug it.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Header from "./Header";
 import BrewList from "./BrewList";
-import NewBrewControl from './components/NewBrewControl';
-import NewBrewForm from './components/NewBrewForm';
+import NewBrewControl from './NewBrewControl';
+import NewBrewForm from './NewBrewForm';
 import { Switch, Route } from 'react-router-dom';
 
 
 const App = () => (
-
-  return (
     <NewBrewControl />
 
   );
